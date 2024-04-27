bef29300c63649e005966b9bf4cdd1b633113fcc
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2024-04-26 17:30:08 -0700

    Refactor #popular_links

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -23,4 +23,44 @@ class TagTopic < ApplicationRecord
       "#{url.short_url}: #{url.num_clicks} visits"
     end
   end
+
+  # A lot of direct SQL, much preferable to the huge number of queries the above method generates.
+  # ShortenedUrl.find_by_sql will return an array of ShortenedUrl objects with properties/methods based
+  # on the values the SQL returns
+  def popular_links2
+    sql = <<-SQL.squish
+      SELECT shortened_urls.short_url, COUNT(*) AS count
+      FROM shortened_urls
+      INNER JOIN taggings ON taggings.url_id = shortened_urls.id
+      INNER JOIN tag_topics ON taggings.topic_id = tag_topics.id
+      INNER JOIN visits ON visits.url_id = shortened_urls.id
+      WHERE tag_topics.id = #{id}
+      GROUP BY shortened_urls.id
+      ORDER BY COUNT(*) DESC
+      LIMIT(3);
+    SQL
+    # result = ApplicationRecord.connection.execute(sql) also works
+    result = ShortenedUrl.find_by_sql(sql)
+    result.map { |url| "#{url.short_url}: #{url.count} visits" }
+  end
+  
+
+  # A more rails-y way of doing it, but complicated to get the right order/syntax. And the syntax seems
+  # brittle/dependent on what version I'm using
+  # https://github.com/rails/rails/issues/36022
+  def popular_links3
+    result = ShortenedUrl
+      .joins(:tag_topics, :visits)
+      .where('tag_topics.id = ?', id)
+      .group('shortened_urls.short_url')
+      .order(count: :desc)
+      .limit(3)
+      .count
+      
+    result.map { |url, count| "#{url}: #{count} visits" }
+  end
+  # count has to come last https://api.rubyonrails.org/v7.1.3.2/classes/ActiveRecord/Calculations.html#method-i-count
+  # the return value after using count is kind of strange. the key string depends on the grouping
+  # the syntax for .order(:count) might fail in other versions
 end
+
